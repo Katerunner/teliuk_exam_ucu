af11a566fdc35f3e09839593b693c9481f188771
Артемій <44300059+Katerunner@users.noreply.github.com>
2019-12-05 13:50:13 +0200

    1

--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,17 +1,24 @@
 package json;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
+    private ArrayList<JsonPair> cache = new ArrayList<>();
 
     public JsonObject(JsonPair... jsonPairs) {
-        // ToDo
+        cache.addAll(Arrays.asList(jsonPairs));
     }
 
     @Override
     public String toJson() {
-        // ToDo
+        String toRes;
+        for (JsonPair pair : cache) {
+
+        }
         return null;
     }
 
