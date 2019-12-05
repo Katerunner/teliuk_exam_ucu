688b3628f80d37a127b557e2f151a6063ba97000
Артемій <44300059+Katerunner@users.noreply.github.com>
2019-12-05 14:51:46 +0200

    4

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -2,21 +2,29 @@ package domain;
 
 import json.*;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class Student extends BasicStudent {
+    private Tuple<String, Integer>[] exams;
 
     public Student(String name, String surname, Integer year, Tuple<String, Integer>... exams) {
-
-        // ToDo
+        super(name, surname, year);
+        this.exams = exams;
     }
 
     public JsonObject toJsonObject() {
-        // ToDo
-        return null;
+        JsonObject toRet = new JsonObject();
+        toRet.add(new JsonPair("name", new JsonString(name)));
+        toRet.add(new JsonPair("surname", new JsonString(surname)));
+        toRet.add(new JsonPair("year", new JsonNumber(year)));
+        ArrayList<JsonPair> jPairs = new ArrayList<>();
+        for (Tuple<String, Integer> pair : exams) {
+            toRet.add(new JsonPair(pair.key, new JsonNumber(pair.value)));
+        }
+//        System.out.println(toRet.toJson());
+        return toRet;
     }
 }
